70c10acb24b52fff32dbda6be1bb1fc5aecd7707
john fellman <john.fellman@gmail.com>
2017-09-28 15:43:17 -0700

    feat(analyzer): add working basic analyzer with play/stop

---
+++ app/components/audio-visualizer.js
@@ -0,0 +1,184 @@
+import Ember from 'ember';
+
+const NUM_CUBES   = 48
+const COLOR       = 0xcc0000
+const OFFSET      = -5
+const TOTAL_WIDTH = 10
+const WIDTH       = TOTAL_WIDTH/NUM_CUBES
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 )
+
+camera.position.z = 5
+
+export default Ember.Component.extend({
+  /**
+   * load and play tracks on init
+   */
+  init() {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext
+    this.audioCtx = new AudioContext()
+
+    let bufferLoader = new BufferLoader(
+      this.audioCtx,
+      [
+        './samples/guitar_1.mp3',
+        './samples/guitar_2.mp3',
+        './samples/vocal.mp3',
+      ],
+      (bufferList) => {
+        this.bufferList = bufferList
+        let { analyserNode, merger } = buildAudioPath(bufferList.length, this.audioCtx)
+        createVisualizer(analyserNode)
+        this.merger = merger
+      }
+    )
+
+    bufferLoader.load()
+    this._super()
+  },
+
+  actions: {
+    /**
+     * plays all the tracks from the beginning
+     */
+    togglePlay() {
+      if (this.playToggleLabel === 'play') {
+        // create the tracks
+        this.tracks = createTracks(this.bufferList, this.audioCtx)
+
+        // connect the tracks to the merger
+        this.tracks.forEach(track => track.connect(this.merger))
+        
+        // start em up
+        this.tracks.forEach(track => track.start(0))
+
+        // set playToggleLabel
+        this.set('playToggleLabel', 'stop')
+      }
+      else {
+        // stop them!
+        this.tracks.forEach(track => track.stop(0))
+
+        // set playToggleLabel
+        this.set('playToggleLabel', 'play')
+      }
+    }
+  },
+
+  playToggleLabel: 'play'
+});
+
+/**
+ * builds the audio path
+ * @private
+ * @param {Number} numChannels
+ * @param {AudioContext} audioCtx
+ * @return {}
+ */
+function buildAudioPath(numChannels, audioCtx) {
+  // create the analyserNode
+  let analyserNode = audioCtx.createAnalyser()
+  analyserNode.fftSize = 2048
+  analyserNode.smoothingTimeConstant = .8;
+
+  // create the merger
+  let merger = audioCtx.createChannelMerger(numChannels)
+
+  // build the path
+  merger.connect(analyserNode)
+  analyserNode.connect(audioCtx.destination)
+
+  return { merger, analyserNode }
+}
+
+/**
+ * creates tracks to be played from a list of audio buffers
+ * @private
+ * @param {Buffer[]} bufferList
+ * @returns {AudioBufferSourceNode[]}
+ */
+function createTracks(bufferList, audioCtx) {
+  return bufferList.map(buffer => {
+    return createTrack(buffer, audioCtx)
+  })
+}
+
+/**
+ * creates an individual audio track
+ * @private
+ * @param {Buffer} buffer
+ * @returns {AudioBufferSourceNode}
+ */
+function createTrack(buffer, audioCtx) {
+    let source = audioCtx.createBufferSource()
+    source.buffer = buffer;
+    return source
+}
+
+function createVisualizer(analyserNode) {
+  // crete the scene
+  let scene = new THREE.Scene()
+
+  // add some lighting
+  var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 )
+  scene.add(light)
+
+  let canvas = document.getElementsByTagName('canvas')[0]
+  if (canvas)
+    canvas.remove()
+
+  var renderer = new THREE.WebGLRenderer()
+  renderer.setSize( window.innerWidth, window.innerHeight )
+  document.body.appendChild( renderer.domElement )
+
+  let cubes = addCubes(analyserNode, scene)
+
+  var animate = function () {
+    requestAnimationFrame( animate )
+    updateCubes(analyserNode, cubes)
+    renderer.render(scene, camera)
+  }
+
+  animate()
+}
+
+function addCubes(analyserNode, scene) {
+  let cubes = []
+
+  for (let i = 0; i < NUM_CUBES; i++) {
+    let offset = OFFSET + (i * WIDTH)
+    cubes.push(addCube(scene, offset, WIDTH, COLOR, i))
+  }
+
+  return cubes
+}
+
+function addCube(scene, offset, width, color, idx) {
+  var geometry = new THREE.BoxGeometry( width, width, width )
+  var material = new THREE.MeshPhongMaterial( { color: color + (idx * 5) } )
+  var cube = new THREE.Mesh( geometry, material )
+  cube.position.set(offset, 0, 0) 
+  scene.add( cube )
+  cube.rotation.x = 0.3
+  cube.rotation.y = 0.3
+  return cube
+}
+
+function updateCubes(analyserNode, cubes) {
+  let freqByteData = new Uint8Array(analyserNode.frequencyBinCount)
+  analyserNode.getByteFrequencyData(freqByteData) 
+  let bucketSize = analyserNode.frequencyBinCount/NUM_CUBES
+
+  for (let i = 0; i < NUM_CUBES; i++) {
+    let freq = i * bucketSize
+    let cube = cubes[i]
+    let avg = freqByteData.slice(freq, (i + 1) * bucketSize)
+      .reduce((a, b) => a + b)/bucketSize
+
+    updateCube(cube, avg, freq + 20)
+  }
+}
+
+function updateCube(cube, value, freq) {
+  cube.position.y = (value/255) * 3
+  cube.rotation.y += 0.1
+}
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,10 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
-  });
+  })
+
+  app.import('vendor/BufferLoader.js')
+  app.import('vendor/three.min.js')
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
@@ -21,5 +24,5 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return app.toTree();
-};
+  return app.toTree()
+}
